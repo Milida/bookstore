37b5b42ab8b79adca7a22208b09aa8a27732f7ca
mksawic <mksawic@gmail.com>
2020-11-27 20:09:09 +0100

    Handling books exception

--- src/main/java/com/sbd/controller/BookController.java
+++ src/main/java/com/sbd/controller/BookController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
 
     @Autowired
@@ -25,19 +27,19 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @GetMapping("/books")
+    @GetMapping
     ResponseEntity<List<Book>> getBooks() {
-        return new ResponseEntity<>(bookRepository.findAll(), HttpStatus.OK);
+        throw new BookNotFoundException("Book not found");
     }
 
-    @PostMapping("/books")
+    @PostMapping
     ResponseEntity<Book> addBook(@RequestBody Book book) {
         return new ResponseEntity<>(bookRepository.save(book), HttpStatus.CREATED);
     }
 
-    @GetMapping("/books/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<Book> getBook(@PathVariable Long id) {
-        Book book = bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException(id));
+        Book book = bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException("Book not found"));
         return new ResponseEntity<>(book, HttpStatus.OK);
     }
 }
--- src/main/java/com/sbd/model/Publisher.java
+++ src/main/java/com/sbd/model/Publisher.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,7 +21,7 @@ public class Publisher {
     @Column(length = 63, nullable = false)
     private String name;
 
-    @OneToMany(mappedBy = "publisher", orphanRemoval = true)
+    @OneToMany(mappedBy = "publisher", orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Book> books = new ArrayList<>();
 
     public Publisher() {
