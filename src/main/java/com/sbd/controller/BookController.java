06659c27d2e2e4b12bb7d0d8d121967b0da57ff5
mksawic <mksawic@gmail.com>
2020-11-27 19:04:06 +0100

    BookController improvement

--- src/main/java/com/sbd/controller/BookController.java
+++ src/main/java/com/sbd/controller/BookController.java
@@ -2,43 +2,42 @@ package com.sbd.controller;
 
 import com.sbd.bookstore.repository.BookRepository;
 import com.sbd.model.Book;
+import com.sbd.payroll.BookNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 import java.util.List;
 
-@Controller
-@RequestMapping()
+@RestController
 public class BookController {
+
     @Autowired
     private BookRepository bookRepository;
 
-    @RequestMapping(path="/book/add/{title}/{price}/{quantity}/{description}")
-    public @ResponseBody
-    ResponseEntity<List<Book>> addNewBook(@PathVariable String title
-            , @PathVariable BigDecimal price, @PathVariable Integer quantity, @PathVariable String description) {
-        Book n = new Book();
-        n.setTitle(title);
-        n.setPrice(price);
-        n.setQuantity(quantity);
-        n.setDescription(description);
-        //n.setPublisher()
-        //n.addCategory()
-        //n.addAuthor()
-        bookRepository.save(n);
-        return new ResponseEntity<>(bookRepository.findAll(), HttpStatus.CREATED);
+    BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
-    @GetMapping(path="/books")
-    public @ResponseBody
-    ResponseEntity<List<Book>> getAllBooks() {
+    @GetMapping("/books")
+    ResponseEntity<List<Book>> getBooks() {
         return new ResponseEntity<>(bookRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/books")
+    ResponseEntity<Book> addBook(@RequestBody Book book) {
+        return new ResponseEntity<>(bookRepository.save(book), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/books/{id}")
+    ResponseEntity<Book> getBook(@PathVariable Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException(id));
+        return new ResponseEntity<>(book, HttpStatus.OK);
     }
 }
--- src/main/java/com/sbd/controller/PublisherController.java
+++ src/main/java/com/sbd/controller/PublisherController.java
@@ -1,7 +1,6 @@
 package com.sbd.controller;
 
 import com.sbd.bookstore.repository.PublisherRepository;
-import com.sbd.model.Author;
 import com.sbd.model.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
